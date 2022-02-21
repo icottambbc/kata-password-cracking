769fc0ee80c53516f0fdbfbc6f72af22facb8cfa
icottambbc <ian.cottam@bbc.co.uk>
2022-02-21 14:53:51 +0000

    Add code for step 1

--- index.test.ts
+++ index.test.ts
@@ -1,7 +1,24 @@
-import { scrambler } from "./index";
+import { scrambler, capsIt } from "./index";
+
+test('capsIt', () => {
+  expect(capsIt("PAssWOrD")).toBe("PASSWORD");
+})
+
+test('capsIt', () => {
+  expect(capsIt("gi1bReI54?)")).toBe("GI1BREI54?)");
+})
+
+test('capsIt', () => {
+  expect(capsIt("PAssWOrD")).toBe("PASSWORD");
+})
+
 
 test('adds 1 + 2 to equal 3', () => {
   expect(scrambler("PASSWORD")).toBe("SSKCYMDG");
+});
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(scrambler("PAssWORD123!")).toBe("SSKCYMDG");
 });
 
 test('adds 1 + 2 to equal 3', () => {
--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
 
-export const scrambler = (password: string): string => {
+export const capsIt = (password: string): string => {
+  let capsPwd = "";
+  for (let i=0; i<password.length; i++) {
+    if (password.charCodeAt(i) >= 97 && password.charCodeAt(i) <= 122) {
+      capsPwd = capsPwd + password[i].toUpperCase();
+    } else {
+      capsPwd = capsPwd + password[i];
+    }
+  }
+  return capsPwd;
+}
 
-  return 'ABC';
+export const scrambler = (password: string): string => {
+  
+  let crackedPW;
+  crackedPW = capsIt(password);
+  return crackedPW;
+
 }
